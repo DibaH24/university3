df533d0a72465c8ff8f8c738a0215c2d04362fed
DibaH24 <diba.print.school@email.com>
2025-03-06 21:07:24 +0330

    add function

---
+++ add.h
@@ -0,0 +1,10 @@
+//
+// Created by arezo on 3/6/2025.
+//
+#include "iostream"
+#include "add.cpp"
+int add(int a, int b)
+#ifndef UNIVERSITY3_ADD_H
+#define UNIVERSITY3_ADD_H
+
+#endif //UNIVERSITY3_ADD_H
